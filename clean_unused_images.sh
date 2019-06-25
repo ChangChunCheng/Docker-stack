67bcd46e11649850b4a9ef01edae0e8d806e1074
ChangChunCheng <ChangChunCheng@users.norely.github.com>
2019-06-25 14:30:00 +0800

    fix bug

--- clean_unused_images.sh
+++ clean_unused_images.sh
@@ -1,1 +1,8 @@
-docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
+img_rm=$(docker images | grep "<none>" | awk "{print \$3}")
+
+if [ $? != 0 ]
+then
+    docker rmi -f $img_rm
+else
+    echo "No image be removed."
+fi
